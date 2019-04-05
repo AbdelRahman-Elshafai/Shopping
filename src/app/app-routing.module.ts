3088daac1c2b66d08359f076c42159d3278ea1d1
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-04-05 21:29:53 +0200

    shared folder with input and sibmut component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
+import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login' , component:SignInFormComponent},
+  {path: 'register' , component:SignUpFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
+import { InputComponent } from './components/shared/input/input.component';
+import { SubmitComponent } from './components/shared/submit/submit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInFormComponent,
-    SignUpFormComponent
+    SignUpFormComponent,
+    InputComponent,
+    SubmitComponent
   ],
   imports: [
     BrowserModule,
