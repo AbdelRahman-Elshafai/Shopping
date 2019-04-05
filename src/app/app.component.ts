ff80c63e305c273dbf8b6a531d7e6dd974e2351e
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-04-06 00:48:54 +0200

    successfully sending data through post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SendDataService } from './services/SendData/send-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Shopping';
+
+  constructor(private sendData:SendDataService){}
+  ngOnInit(){
+    this.sendData.post_product();
+  }
 }
--- src/app/services/SendData/send-data.service.ts
+++ src/app/services/SendData/send-data.service.ts
@@ -81,19 +81,15 @@ export class SendDataService {
       image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
       amount: 10,
       price : 956
-    },
-    {
-      id: "HT-1000",
-      name: "Notebook Basic 15",
-      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
-      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
-      amount: 10,
-      price : 956
     }
   ];
   constructor(private httpClient:HttpClient) { }
 
   post_product(){
-
+    return this.httpClient.post('https://jsonplaceholder.typicode.com/posts', this.products).subscribe(
+      (res) => {
+        console.log(res);
+      }
+    );
   }
 }
