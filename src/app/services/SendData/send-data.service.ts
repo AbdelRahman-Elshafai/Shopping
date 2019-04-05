8b40dbc26f347469d07b21269f4bfd16ef307cc8
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-04-06 00:40:32 +0200

    adding products to the send data service

--- src/app/services/SendData/send-data.service.ts
+++ src/app/services/SendData/send-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Product } from 'src/app/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,89 @@ import { HttpClient } from '@angular/common/http';
 export class SendDataService {
 
   url = "http://localhost:4200/home";
+
+  private products:Product[] = [
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    },
+    {
+      id: "HT-1000",
+      name: "Notebook Basic 15",
+      description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+      image : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+      amount: 10,
+      price : 956
+    }
+  ];
   constructor(private httpClient:HttpClient) { }
 
   post_product(){
