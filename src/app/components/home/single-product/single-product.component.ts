ab4446c48e3ba5edc766e967ced24a132f50f82a
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-04-07 02:09:25 +0200

    showing the nine products in the homepage

--- src/app/components/home/single-product/single-product.component.ts
+++ src/app/components/home/single-product/single-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-single-product',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-product.component.scss']
 })
 export class SingleProductComponent implements OnInit {
-
+  @Input('singleProduct') product;
   constructor() { }
 
   ngOnInit() {
