cdeee0493f7038ae9c3a78e3867548801f6cdbf2
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-04-09 07:24:06 +0200

    creating the x button in the cart-counter component

--- src/app/components/header/cart-counter/cart-counter.component.ts
+++ src/app/components/header/cart-counter/cart-counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cart-counter',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-counter.component.scss']
 })
 export class CartCounterComponent implements OnInit {
+  @ViewChild('dropDown') dropDown : ElementRef;
 
-  constructor() { }
+  constructor(private renderer:Renderer2 , private el :ElementRef) { }
 
   ngOnInit() {
+    const anchor = this.renderer.createElement('a');
+    const text = this.renderer.createText('hello world');
+    this.renderer.appendChild(anchor , text);
+    this.renderer.appendChild(this.dropDown.nativeElement , anchor);
+    // this.createAnchor();
+    this.createXButton();
+  }
+
+
+  createXButton(){
+    const button = this.renderer.createElement('button');
+    this.renderer.setAttribute(button , 'type' , 'button');
+    this.renderer.setAttribute(button , 'aria-label' , 'Close');
+    this.renderer.addClass(button , 'close');
+    const span = this.renderer.createElement('span');
+    this.renderer.setAttribute(span , 'aria-hidden' , 'true');
+    const text = this.renderer.createText('×');
+    this.renderer.appendChild(span , text);
+    this.renderer.appendChild(button , span);    
+    this.renderer.appendChild(this.dropDown.nativeElement , button);
   }
 
 }
--- src/app/components/header/navigation-links/navigation-links.component.ts
+++ src/app/components/header/navigation-links/navigation-links.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-links',
@@ -6,30 +6,12 @@ import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/co
   styleUrls: ['./navigation-links.component.scss']
 })
 export class NavigationLinksComponent implements OnInit {
-  @ViewChild('dropDown') dropDown : ElementRef;
-  constructor(private renderer:Renderer2 , private el :ElementRef) { }
+  constructor() { }
 
   ngOnInit() {
     
-    const anchor = this.renderer.createElement('a');
-    const text = this.renderer.createText('hello world');
-    this.renderer.appendChild(anchor , text);
-    this.renderer.appendChild(this.dropDown.nativeElement , anchor);
-    // this.createAnchor();
-    this.createXButton();
+
   }
 
-  createXButton(){
-    const button = this.renderer.createElement('button');
-    this.renderer.setAttribute(button , 'type' , 'button');
-    this.renderer.setAttribute(button , 'aria-label' , 'Close');
-    this.renderer.addClass(button , 'close');
-    const span = this.renderer.createElement('span');
-    this.renderer.setAttribute(span , 'aria-hidden' , 'true');
-    const text = this.renderer.createText('x');
-    this.renderer.appendChild(span , text);
-    this.renderer.appendChild(button , span);    
-    this.renderer.appendChild(this.dropDown.nativeElement , button);
-  }
 
 }
