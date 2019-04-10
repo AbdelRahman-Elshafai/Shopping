8e1ccc207dac426d4896f4784d41761ab27d63a5
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-04-10 19:43:48 +0200

    show log out in navigation link

--- src/app/components/header/navigation-links/navigation-links.component.ts
+++ src/app/components/header/navigation-links/navigation-links.component.ts
@@ -6,9 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-links.component.scss']
 })
 export class NavigationLinksComponent implements OnInit {
+  loggedIn : boolean = false;
+  email : string;
   constructor() { }
 
   ngOnInit() {
+    if(localStorage.getItem('User')){
+      
+      this.loggedIn = true;
+      this.email = JSON.parse(localStorage.getItem('User')).email;
+      console.log(this.email);
+    }
+    else{
+      this.loggedIn = false;
+    }
     
 
   }
