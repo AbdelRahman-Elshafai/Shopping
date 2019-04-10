fedb55e6ff7ee875b2133d2b76099bc673bfbe85
AbdelRahman-Elshafai <abdelrahman.elshafai@gmail.com>
2019-04-10 16:33:35 +0200

    styling home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ProductsService } from 'src/app/services/Products/products.service';
 import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation:ViewEncapsulation.None
+
 })
 export class HomeComponent implements OnInit {
 
